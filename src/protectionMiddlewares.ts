600c7d7434267a04abc122a2bc8ac943adc7e045
Yash Goyal <yashgo0018@gmail.com>
2023-02-11 01:41:52 +0530

    added protection middleware

---
+++ src/protectionMiddlewares.ts
@@ -0,0 +1,83 @@
+import { Organization, PrismaClient } from "@prisma/client";
+import { NextFunction, Request, Response } from "express";
+import { verifyJWTToken } from "./helpers";
+
+const prisma = new PrismaClient();
+
+export async function onlyAuthorized(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const { authorization } = req.headers;
+  if (!authorization) {
+    return res.status(400).json({ message: "authorization token not found" });
+  }
+  if (
+    !(
+      authorization.startsWith("Bearer") && authorization.split(" ").length == 2
+    )
+  ) {
+    return res
+      .status(400)
+      .json({ message: "authorization token is in invalid format" });
+  }
+  const token = authorization.split(" ")[1];
+  const decoded = verifyJWTToken(token) as {
+    username: string;
+    type: "CELEB" | "ORG";
+  };
+  if (!decoded) {
+    return res.status(401).json({ message: "Invalid authorization token" });
+  }
+  (req as any).userType = decoded.type;
+  if (decoded.type == "ORG") {
+    const organization = await prisma.organization.findUnique({
+      where: { admin: decoded.username },
+    });
+    if (!organization)
+      return res.status(401).json({
+        message:
+          "The organization associated to this authorization token not found",
+      });
+    (req as any).organization = organization;
+    return next();
+  }
+
+  // TODO: make celeb flow
+  console.error("Celeb auth flow not implemented yet");
+}
+
+export async function onlyAuthorizedOrganization(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  onlyAuthorized(req, res, () => {
+    const { type } = req as Request & {
+      type: "CELEB" | "ORG";
+    };
+    if (type != "ORG")
+      return res
+        .status(401)
+        .json({ message: "The route is only for registered organizations" });
+    next();
+  });
+}
+
+export async function onlyAuthorizedCeleb(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  onlyAuthorized(req, res, () => {
+    const { type } = req as Request & {
+      type: "CELEB" | "ORG";
+    };
+    if (type != "CELEB")
+      return res
+        .status(401)
+        .json({ message: "The route is only for registered celebrities" });
+    next();
+  });
+}

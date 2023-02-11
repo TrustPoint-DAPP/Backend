4c553a6c04ffcb73fda0c20201d1bf8f22caa20b
Yash Goyal <yashgo0018@gmail.com>
2023-02-11 21:06:00 +0530

    added more env variables

--- src/config.ts
+++ src/config.ts
@@ -3,11 +3,12 @@ import { ethers } from "ethers";
 
 dotenv.config();
 
-export const PORT = parseInt(process.env.PORT || "8000");
+export const PORT = Number(process.env.PORT || "8000");
 export const ORGANIZATION_CONTRACT_ADDRESS =
   process.env.ORGANIZATION_CONTRACT_ADDRESS || "";
 export const DEAL_CONTRACT_ADDRESS = process.env.DEAL_CONTRACT_ADDRESS || "";
-export const NFT_CONTRACT_ADDRESS = process.env.NFT_CONTRACT_ADDRESS || "";
+export const LOGGER_CONTRACT_ADDRESS =
+  process.env.LOGGER_CONTRACT_ADDRESS || "";
 export const WEB3_RPC_URI = process.env.WEB3_RPC_URI || "";
 export const SIGNER_PRIVATE_KEY = process.env.SIGNER_PRIVATE_KEY || "";
 export const LIGHTHOUSE_API_KEY = process.env.LIGHTHOUSE_API_KEY || "";
@@ -15,6 +16,7 @@ export const CHAIN_ID = process.env.CHAIN_ID || 3141;
 export const NONCE_TEMPLATE = process.env.NONCE_MESSAGE || "The Nonce is: %";
 export const CHANNEL_PK = process.env.CHANNEL_PK || "";
 export const JWT_SECRET = process.env.JWT_SECRET || "";
+export const MIN_BLOCK = Number(process.env.MIN_BLOCK || "0");
 
 export const provider = new ethers.providers.JsonRpcProvider(
   WEB3_RPC_URI,

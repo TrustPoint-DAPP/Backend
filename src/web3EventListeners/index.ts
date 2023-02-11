2afc69cbbb8fbec12c5a893fa87e85924f9ab6ee
Yash Goyal <yashgo0018@gmail.com>
2023-02-11 21:06:32 +0530

    create web3 event listener setup

---
+++ src/web3EventListeners/index.ts
@@ -0,0 +1,35 @@
+import { ethers } from "ethers";
+import {
+  DEAL_CONTRACT_ADDRESS,
+  LOGGER_CONTRACT_ADDRESS,
+  ORGANIZATION_CONTRACT_ADDRESS,
+  provider,
+} from "../config";
+import ORGANIZATION_CONTROLLER_ABI from "../abi/OrganizationController.json";
+import DEAL_CONTROLLER_ABI from "../abi/DealController.json";
+import LOGGER_ABI from "../abi/Logger.json";
+import {
+  DealController,
+  Logger,
+  OrganizationController,
+} from "../typechain-types";
+
+const organizationController = new ethers.Contract(
+  ORGANIZATION_CONTRACT_ADDRESS,
+  ORGANIZATION_CONTROLLER_ABI,
+  provider
+) as OrganizationController;
+
+const dealController = new ethers.Contract(
+  DEAL_CONTRACT_ADDRESS,
+  DEAL_CONTROLLER_ABI,
+  provider
+) as DealController;
+
+const logger = new ethers.Contract(
+  LOGGER_CONTRACT_ADDRESS,
+  LOGGER_ABI,
+  provider
+) as Logger;
+
+export default async function web3EventListeners() {}
